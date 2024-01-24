d394ba7fd90f9c84ae06a6bde87890c751a90870
Mahabub Islam <miprio101@gmail.com>
2024-01-24 17:16:41 +0600

    feat: fetcher for the dog breed.

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,25 +1,43 @@
 class HomesController < ApplicationController
+  before_action :validate_search_params, only: :search
+
   def index
   end
 
   def search
-    response = HTTParty.get("https://dog.ceo/api/breed/#{search_params[:query]}/images/random")
+    query_downcased = search_params[:query].downcase
+    response = HTTParty.get("https://dog.ceo/api/breed/#{query_downcased}/images/random")
 
-    if response.code == 200
+
+    if success_response?(response["status"])
       @image = response['message']
-    elsif response.code == 404
+
+    elsif error_response?(response["status"])
       flash[:error] = 'Please enter a valid dog breed...'
+    else
+      flash[:error] = 'Internal server error...'
+      redirect_to root_path
     end
   end
 
   private
 
+  def validate_search_params
+    if params[:search][:query].blank?
+      flash[:error] = 'Please enter a dog breed...'
+      return redirect_to root_path
+    end
+  end
+
+  def success_response?(status)
+    status == "success".freeze
+  end
+
+  def error_response?(status)
+    status == "error".freeze
+  end
+
   def search_params
-    raise ActionController::ParameterMissing if params[:search].blank?
-
     params.require(:search).permit(:query)
-
-  rescue ActionController::ParameterMissing
-    flash[:error] = 'Please enter a search query, To search the dog breed'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "homes#index"
+
+  resources :homes, only: [:index] do
+    collection do
+      get :search
+    end
+  end
 end

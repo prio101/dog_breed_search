9f4b1dee6af44e8c6220ac2e480724f77d1ad9a1
Mahabub Islam <miprio101@gmail.com>
2024-01-25 14:46:36 +0600

    spec: added the spec for request controller and helper written

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -1,2 +1,7 @@
 module HomesHelper
+  # Format breed name for url with hyphens and makes it lowercase
+  # example: "German Shepherd" => "german-shepherd"
+  def breed_name_formatted(breed_name)
+    breed_name.downcase.split.join('-')
+  end
 end
